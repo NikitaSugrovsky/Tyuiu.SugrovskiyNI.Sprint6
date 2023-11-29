cc2248da7c850263a26900da4253ce6186bcb437
Nikita Sugrovsky <sugrovskiy01@bk.ru>
2023-11-29 22:49:29 +0500

    Task1

--- Tyuiu.SugrovskiyNI.Sprint6.Task1.V8.Test/DataServiceTest.cs
+++ Tyuiu.SugrovskiyNI.Sprint6.Task1.V8.Test/DataServiceTest.cs
@@ -11,11 +11,36 @@ namespace Tyuiu.SugrovskiyNI.Sprint6.Task1.V8.Test
         public void ValidGetMassFunction()
         {
             DataService ds = new DataService();
+
             int startValue = -5;
             int stopValue = 5;
-            double[] res = ds.GetMassFunction(startValue, stopValue);
-            double wait = 23;
-            Assert.AreEqual(wait, res);
+
+            int len = stopValue - startValue + 1;
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = -36.41;
+            valueWaitArray[1] = -22.07;
+            valueWaitArray[2] = 5.68;
+            valueWaitArray[3] = 16.72;
+            valueWaitArray[4] = 8.35;
+            valueWaitArray[5] = -0.50;
+            valueWaitArray[6] = 9.63;
+            valueWaitArray[7] = 16.29;
+            valueWaitArray[8] = 5.01;
+            valueWaitArray[9] = -22.40;
+            valueWaitArray[10] = -36.30;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+
+            // Округляем значения функции в массиве valueWaitArray перед сравнением с массивом res
+            for (int i = 0; i < valueWaitArray.Length; i++)
+            {
+                valueWaitArray[i] = Math.Round(valueWaitArray[i], 2);
+            }
         }
     }
 }
